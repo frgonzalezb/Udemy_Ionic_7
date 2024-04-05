0586cce0e2657c0489295342523df22b69627aaa
Francisco <fr.gonzalezb@duocuc.cl>
2024-04-04 21:27:37 -0300

    Extras de producto (varias opciones)

--- 06_BurgerQueenApp/src/app/pages/product/product.page.ts
+++ 06_BurgerQueenApp/src/app/pages/product/product.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from '@ionic/angular';
 import Product from 'src/app/models/product';
+import ProductExtraOption from 'src/app/models/product-extra-option';
 
 @Component({
   selector: 'app-product',
@@ -23,6 +24,16 @@ export class ProductPage implements OnInit {
     if (!this.product) {
       this.navController.navigateForward('categories');
     }
+  }
+
+  changeMultipleOption($event: any, options: ProductExtraOption[]) {
+    console.log($event); // dbg
+    
+    options.forEach(op => op.activate = $event.detail.value == op.name);
+
+    console.log(options); // dbg
+    console.log(this.product); // dbg
+    
   }
 
 }
