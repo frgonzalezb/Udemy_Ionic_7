146ad5a3af88b7345ea5aad747320e71689ade75
Francisco <fr.gonzalezb@duocuc.cl>
2024-05-27 16:01:40 -0400

    Haciendo login desde nuestro formulario

--- 06_BurgerQueenApp/src/app/shared/login/login.component.ts
+++ 06_BurgerQueenApp/src/app/shared/login/login.component.ts
@@ -1,8 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { Store } from '@ngxs/store';
+import { ToastService } from 'src/app/services/toast.service';
+import { Login } from 'src/app/state/auth/auth.actions';
+import { AuthState } from 'src/app/state/auth/auth.state';
 import User from 'src/app/models/user';
 
 @Component({
@@ -22,7 +26,11 @@ export class LoginComponent {
 
   public user: User;
 
-  constructor() {
+  constructor(
+    private store: Store,
+    private _toast: ToastService,
+    private _translate: TranslateService
+  ) {
     this.user = new User();
     this.newAccount = new EventEmitter<boolean>();
     this.back = new EventEmitter<boolean>();
@@ -30,7 +38,23 @@ export class LoginComponent {
   }
 
   login() {
-
+    this.store.dispatch(new Login({
+      email: this.user.email,
+      password: this.user.password
+    })).subscribe({
+      next: () => {
+        const success = this.store.selectSnapshot(AuthState.success);
+        if (success) {
+          this._toast.showToast(this._translate.instant('label.login.success'));
+          this.doLogin.emit(true);
+        } else {
+          this._toast.showToast(this._translate.instant('label.login.error'));
+        }
+      }, error: (e) => {
+        console.error(e);
+        this._toast.showToast(this._translate.instant('label.login.error'));
+      }
+    });
   }
 
   exit() {
