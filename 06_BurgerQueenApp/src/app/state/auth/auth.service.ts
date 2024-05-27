96305969a32c664ac0d82f36c187e29065457c81
Francisco <fr.gonzalezb@duocuc.cl>
2024-05-27 13:28:34 -0400

    Update auth.service.ts

--- 06_BurgerQueenApp/src/app/state/auth/auth.service.ts
+++ 06_BurgerQueenApp/src/app/state/auth/auth.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { CapacitorHttp, HttpResponse } from '@capacitor/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,27 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  login(email: string, password: string) {
+    return CapacitorHttp.post({
+      url: environment.urlApi + 'auth/login',
+      data: {
+        email,
+        password
+      },
+      params: {},
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response: HttpResponse) => {
+      if (response.status == 201) {
+        const data = response.data as any;
+        return data;
+      }
+      return null;
+    }).catch(error => {
+      console.error(error);
+      return null;
+    });
+  }
 }
