9c9350b618fa25edae1f7ef86369f5768139ea00
Francisco <fr.gonzalezb@duocuc.cl>
2024-06-10 23:52:42 -0400

    Seteando las traducciones

--- 07_ClassManager/src/app/app.component.ts
+++ 07_ClassManager/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Device } from '@capacitor/device';
+import { Platform } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private _translate: TranslateService
+  ) {
+    this._translate.setDefaultLang('es');
+    this.initApp();
+  }
+
+  initApp() {
+    this.platform.ready().then(async () => {
+      const language = await Device.getLanguageCode();
+      if (language.value) {
+        this._translate.use(language.value.slice(0, 2));
+        // this._translate.setDefaultLang(language.value);
+      }
+    });
+  }
 }
