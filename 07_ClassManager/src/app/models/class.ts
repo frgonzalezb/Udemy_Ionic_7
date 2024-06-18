7d07a85dd221f98eea0aa7262d55e3ddede3375b
Francisco <fr.gonzalezb@duocuc.cl>
2024-06-17 23:15:59 -0400

    Modelo clase

---
+++ 07_ClassManager/src/app/models/class.ts
@@ -0,0 +1,10 @@
+class Class {
+  id!: number;
+  date_start!: string;
+  date_end!: string;
+  id_student!: number;
+  price!: number;
+  active!: number;  // SQLite usa 1 y 0 para true y false, respectivamente
+}
+
+export default Class;
