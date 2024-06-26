02403047452fb4e20e49fa304760f4c831d0afb5
Francisco <fr.gonzalezb@duocuc.cl>
2024-06-26 16:18:55 -0400

    ToastService y AlertService

---
+++ 08_DdrEvents/src/app/services/alert.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+
+  constructor(
+    private alertCtrl: AlertController,
+    private _translate: TranslateService
+  ) { }
+
+  async alertSuccess(header: string, message: string) {
+    const alert = await this.alertCtrl.create({
+      header: header,
+      message: message,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  async alertConfirm(header: string, message: string, functionOk: Function) {
+    const alert = await this.alertCtrl.create({
+      header: header,
+      message: message,
+      buttons: [
+        {
+          text: this._translate.instant('label.cancel'),
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        },
+        {
+          text: this._translate.instant('label.ok'),
+          role: 'confirm',
+          cssClass: 'secondary',
+          handler: () => {
+            functionOk();
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+}
