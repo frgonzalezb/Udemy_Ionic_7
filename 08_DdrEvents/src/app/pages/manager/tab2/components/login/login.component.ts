97e44a2c4d930a0294729d222e7900cd6d3b8141
Francisco <fr.gonzalezb@duocuc.cl>
2024-06-26 19:33:15 -0400

    Formulario Login HTML

--- 08_DdrEvents/src/app/pages/manager/tab2/components/login/login.component.ts
+++ 08_DdrEvents/src/app/pages/manager/tab2/components/login/login.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import User from 'src/app/models/user';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent  implements OnInit {
+export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public user: User;
+
+  constructor() {
+    this.user = new User();
+  }
 
   ngOnInit() {}
+
+  login() {
+
+  }
 
 }
