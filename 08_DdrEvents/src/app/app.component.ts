597330b50f519c72830c361414811018a3608ff2
Francisco <fr.gonzalezb@duocuc.cl>
2024-06-26 13:09:03 -0400

    Seteando las traducciones

--- 08_DdrEvents/src/app/app.component.ts
+++ 08_DdrEvents/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Device } from '@capacitor/device';
+import { Platform } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private _translate: TranslateService
+  ) {
+    this._translate.setDefaultLang('es');
+    this.initApp();
+  }
+
+  initApp() {
+    this.platform.ready().then(async () => {
+      
+      const language = await Device.getLanguageCode();
+      const info = await Device.getInfo();
+      
+      if (language.value) {
+        this._translate.use(language.value.slice(0, 2));
+      }
+    });
+  }
 }
