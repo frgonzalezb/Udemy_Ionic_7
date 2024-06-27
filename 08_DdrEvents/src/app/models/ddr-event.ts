1e47278380d285e4157b07f0efc18c524bd6f1c5
Francisco <fr.gonzalezb@duocuc.cl>
2024-06-27 12:53:47 -0400

    Modelo EventDDR

---
+++ 08_DdrEvents/src/app/models/ddr-event.ts
@@ -0,0 +1,11 @@
+class DDREvent {
+  id!: string;
+  title!: string;
+  description!: string;
+  dateStart!: string;
+  dateEnd?: string;
+  type!: string;
+  url!: string;
+}
+
+export default DDREvent;
