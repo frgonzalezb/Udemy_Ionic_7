c2e89dc12913cba0c0d131c2bcb76219e873fcb0
Francisco <fr.gonzalezb@duocuc.cl>
2024-06-27 18:47:44 -0400

    Formulario para añadir eventos (parte 1)

--- 08_DdrEvents/src/app/pages/manager/tab2/components/add-edit-events/add-edit-events.component.ts
+++ 08_DdrEvents/src/app/pages/manager/tab2/components/add-edit-events/add-edit-events.component.ts
@@ -5,10 +5,22 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './add-edit-events.component.html',
   styleUrls: ['./add-edit-events.component.scss'],
 })
-export class AddEditEventsComponent  implements OnInit {
+export class AddEditEventsComponent implements OnInit {
 
-  constructor() { }
+  public showEnd: boolean;
+
+  constructor() {
+    this.showEnd = false;
+  }
 
   ngOnInit() {}
+
+  addOrEditEvent() {
+
+  }
+
+  changeShowEnd() {
+    this.showEnd = !this.showEnd;
+  }
 
 }
