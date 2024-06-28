b55a254bfc87f643c347a8cd5a9299a378dd00bb
Francisco <fr.gonzalezb@duocuc.cl>
2024-06-27 22:23:45 -0400

    Formulario reactivo

--- 08_DdrEvents/src/app/pages/manager/tab2/components/add-edit-events/add-edit-events.component.ts
+++ 08_DdrEvents/src/app/pages/manager/tab2/components/add-edit-events/add-edit-events.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import * as moment from 'moment';
+import DDREvent from 'src/app/models/ddr-event';
 
 @Component({
   selector: 'app-add-edit-events',
@@ -9,24 +12,64 @@ export class AddEditEventsComponent implements OnInit {
 
   public showEnd: boolean;
   public edit: boolean;
+  public eventForm!: FormGroup;
+  public event!: DDREvent;
 
-  constructor() {
-    this.showEnd = false;
+  constructor(
+    private fb: FormBuilder
+  ) {
     this.edit = false;
+    this.showEnd = false;
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.initEvent();
+  }
+
+  initEvent() {
+    if (!this.event) {
+      this.edit = false;
+      this.showEnd = false;
+      this.event = new DDREvent();
+    } else {
+      this.edit = true;
+      this.showEnd = (this.event.dateEnd) != null;
+    }
+    this.eventForm = this.fb.group({
+      title: new FormControl(this.event.title, [
+        Validators.required
+      ]),
+      start: new FormControl(this.event.dateStart ? this.event.dateStart : moment().format('YYYY-MM-DDTHH:mm')),
+      end: new FormControl(this.event.dateEnd ? this.event.dateEnd : moment().format('YYYY-MM-DDTHH:mm')),
+      type: new FormControl(this.event.type ? this.event.type : 'blog', [
+        Validators.required
+      ]),
+      url: new FormControl(this.event.url, [
+        Validators.required,
+        Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')
+      ]),
+      description: new FormControl(this.event.description, [
+        Validators.required,
+      ])
+    });
+  }
 
   addOrEditEvent() {
-
+    console.log(this.eventForm.value); // dbg
+    
   }
 
   newEvent() {
-    
+
   }
 
   changeShowEnd() {
     this.showEnd = !this.showEnd;
+    if (this.showEnd) {
+      this.eventForm.patchValue({ end: moment().format('YYYY-MM-DDTHH:mm') });
+    } else {
+      this.eventForm.patchValue({ end: null });
+    }
   }
 
 }
--- 08_DdrEvents/src/app/pages/manager/tab2/tab2.module.ts
+++ 08_DdrEvents/src/app/pages/manager/tab2/tab2.module.ts
@@ -1,7 +1,7 @@
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 import { TranslateModule } from '@ngx-translate/core';
@@ -14,7 +14,8 @@ import { LoginComponent } from './components/login/login.component';
     CommonModule,
     FormsModule,
     Tab2PageRoutingModule,
-    TranslateModule
+    TranslateModule,
+    ReactiveFormsModule
   ],
   declarations: [Tab2Page, AddEditEventsComponent, LoginComponent]
 })
