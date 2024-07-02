075a8762658cda3f5681d3f77449389f8f6bf83f
Francisco <fr.gonzalezb@duocuc.cl>
2024-07-01 23:44:24 -0400

    Mostrar acciones eventos

--- 08_DdrEvents/src/app/pages/events/tab1/components/event-list/event-list.component.ts
+++ 08_DdrEvents/src/app/pages/events/tab1/components/event-list/event-list.component.ts
@@ -14,9 +14,12 @@ Ref: https://www.ngxs.io/deprecations/select-decorator-deprecation
 */
 
 import { Component, OnInit, inject } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 import { Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import DDREvent from 'src/app/models/ddr-event';
+import { AuthState } from 'src/app/state/auth/auth.state';
 import { GetFutureEvents } from 'src/app/state/events/events.actions';
 import { EventsState } from 'src/app/state/events/events.state';
 
@@ -31,7 +34,11 @@ export class EventListComponent  implements OnInit {
 
   public events: DDREvent[];
 
-  constructor(private store: Store) {
+  constructor(
+    private store: Store,
+    private actionSheetCtrl: ActionSheetController,
+    private _translate: TranslateService
+  ) {
     this.events = [];
   }
 
@@ -48,6 +55,52 @@ export class EventListComponent  implements OnInit {
         console.log(this.events); // dbg
       }
     });
+  }
+
+  clickEvent() {
+    const isLoggedIn = this.store.selectSnapshot(AuthState.isLoggedIn);
+    if (isLoggedIn) {
+      this.presentActions();
+    }
+  }
+
+  async presentActions() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: this._translate.instant('label.actions.event'),
+      buttons: [
+        {
+          text: this._translate.instant('label.open.url'),
+          icon: 'earth-outline',
+          handler: () => {
+            // TODO
+            console.log('Open URL clicked!'); // dbg
+          }
+        },
+        {
+          text: this._translate.instant('label.edit.event'),
+          icon: 'pencil-outline',
+          handler: () => {
+            // TODO
+            console.log('Edit event clicked!'); // dbg
+          }
+        },
+        {
+          text: this._translate.instant('label.remove.event'),
+          icon: 'trash-outline',
+          handler: () => {
+            // TODO
+            console.log('Remove event clicked!'); // dbg
+          }
+        },
+        {
+          text: this._translate.instant('label.close.options'),
+          icon: 'close-outline',
+          role: 'cancel'
+        },
+      ]
+    });
+
+    await actionSheet.present();
   }
 
 }
