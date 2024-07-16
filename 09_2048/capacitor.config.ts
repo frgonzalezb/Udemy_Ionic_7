93f91f35b94bcf1b21ca7ca8f3ebf16607306a06
Francisco <fr.gonzalezb@duocuc.cl>
2024-07-16 12:01:17 -0400

    2048 App: Creación del proyecto

---
+++ 09_2048/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '09_2048',
+  webDir: 'www'
+};
+
+export default config;
